69d2c8c73f31f59c99b83d55d3820fd043ea1732
Eroto <seyive.awaya@gmail.com>
2024-07-25 14:03:21 +0100

    updating copy button

--- translationApp/my_translator/lib/main_bloc.dart
+++ translationApp/my_translator/lib/main_bloc.dart
@@ -37,26 +37,40 @@ class _MainBlocState extends State<MainBloc> {
               style: TextStyle(color: Colors.white, fontSize: 16),
               textAlign: TextAlign.justify,
             ),
-            Container(
-              width: 10.0,
-              height: 10.0,
-              margin: EdgeInsetsDirectional.fromSTEB(335, 90, 0, 0),
-              color: Color.fromARGB(255, 22, 22, 22),
-              child: IconButton(iconSize: 10, alignment: Alignment.bottomCenter,
-                onPressed: (){
-                  Clipboard.setData(ClipboardData(text: widget.text));
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Copied to clipboard'),
-                    ),
-                  );
-                },
-                icon: Icon(
-                  Icons.copy, 
-                  color: Colors.white,
+            Expanded(
+              child: Container(
+                width: 10.0,
+                height: 10.0,
+                margin: EdgeInsetsDirectional.fromSTEB(335, 90, 0, 0),
+                color: Color.fromARGB(255, 22, 22, 22),
+                child: IconButton(iconSize: 10, alignment: Alignment.bottomCenter,
+                  onPressed: (){
+                    Clipboard.setData(ClipboardData(text: widget.text));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Copied to clipboard'),
+                      ),
+                    );
+                  },
+                  icon: Icon(
+                    Icons.copy, 
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
+            /*FloatingActionButton(
+              mini: true,
+              onPressed: (){
+                Clipboard.setData(ClipboardData(text: widget.text));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Copied to clipboard'),
+                  ),
+                );
+              },
+              child: Icon(size:10, Icons.copy, color: Colors.white,),
+            )*/
           ],
         ),
       )
